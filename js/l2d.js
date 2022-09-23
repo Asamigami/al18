0cf1a2516064cfecbdcb8ef91c9bc0607a7d7201
marisa <19626-marisa@users.noreply.gitgud.io>
2022-09-23 16:05:28 +0100

    fixed duplicated animations in L2D

--- js/l2d.js
+++ js/l2d.js
@@ -15,7 +15,7 @@ class L2D {
 
         return this;
     }
-    
+   
     load (name, v) {
         if (!this.models[name]) {
             let modelDir = name+'/';
@@ -29,7 +29,7 @@ class L2D {
                 this.loader.add(name+'_model', modelDir+modelPath, { xhrType: PIXI.loaders.Resource.XHR_RESPONSE_TYPE.JSON });
                 modelNames.push(name+'_model');
             //} 
-
+			
             this.loader.load((loader, resources) => {
                 let model3Obj = resources[name+'_model'].data;
                 
@@ -48,6 +48,39 @@ class L2D {
                     loader.add(name+'_physics', modelDir+model3Obj['FileReferences']['Physics'], { xhrType: PIXI.loaders.Resource.XHR_RESPONSE_TYPE.JSON });
                 }
 
+				// START: Load default motions to set proper order and make sure none are missing
+				loader.add(name+'_complete', modelDir+'motions/complete.motion3.json', { xhrType: PIXI.loaders.Resource.XHR_RESPONSE_TYPE.JSON });
+                motionNames.push(name+'_complete');
+				loader.add(name+'_effect', modelDir+'motions/effect.motion3.json', { xhrType: PIXI.loaders.Resource.XHR_RESPONSE_TYPE.JSON });
+                motionNames.push(name+'_effect');
+				loader.add(name+'_home', modelDir+'motions/home.motion3.json', { xhrType: PIXI.loaders.Resource.XHR_RESPONSE_TYPE.JSON });
+                motionNames.push(name+'_home');
+				loader.add(name+'_idle', modelDir+'motions/idle.motion3.json', { xhrType: PIXI.loaders.Resource.XHR_RESPONSE_TYPE.JSON });
+                motionNames.push(name+'_idle');
+				loader.add(name+'_login', modelDir+'motions/login.motion3.json', { xhrType: PIXI.loaders.Resource.XHR_RESPONSE_TYPE.JSON });
+                motionNames.push(name+'_login');
+				loader.add(name+'_mail', modelDir+'motions/mail.motion3.json', { xhrType: PIXI.loaders.Resource.XHR_RESPONSE_TYPE.JSON });
+                motionNames.push(name+'_mail');
+				loader.add(name+'_main_1', modelDir+'motions/main_1.motion3.json', { xhrType: PIXI.loaders.Resource.XHR_RESPONSE_TYPE.JSON });
+                motionNames.push(name+'_main_1');
+				loader.add(name+'_main_2', modelDir+'motions/main_2.motion3.json', { xhrType: PIXI.loaders.Resource.XHR_RESPONSE_TYPE.JSON });
+                motionNames.push(name+'_main_2');
+				loader.add(name+'_main_3', modelDir+'motions/main_3.motion3.json', { xhrType: PIXI.loaders.Resource.XHR_RESPONSE_TYPE.JSON });
+                motionNames.push(name+'_main_3');
+				loader.add(name+'_mission', modelDir+'motions/mission.motion3.json', { xhrType: PIXI.loaders.Resource.XHR_RESPONSE_TYPE.JSON });
+                motionNames.push(name+'_mission');
+				loader.add(name+'_mission_complete', modelDir+'motions/mission_complete.motion3.json', { xhrType: PIXI.loaders.Resource.XHR_RESPONSE_TYPE.JSON });
+                motionNames.push(name+'_mission_complete');
+				loader.add(name+'_touch_body', modelDir+'motions/touch_body.motion3.json', { xhrType: PIXI.loaders.Resource.XHR_RESPONSE_TYPE.JSON });
+                motionNames.push(name+'_touch_body');
+				loader.add(name+'_touch_head', modelDir+'motions/touch_head.motion3.json', { xhrType: PIXI.loaders.Resource.XHR_RESPONSE_TYPE.JSON });
+                motionNames.push(name+'_touch_head');
+				loader.add(name+'_touch_special', modelDir+'motions/touch_special.motion3.json', { xhrType: PIXI.loaders.Resource.XHR_RESPONSE_TYPE.JSON });
+                motionNames.push(name+'_touch_special');
+				loader.add(name+'_wedding', modelDir+'motions/wedding.motion3.json', { xhrType: PIXI.loaders.Resource.XHR_RESPONSE_TYPE.JSON });
+                motionNames.push(name+'_wedding');
+				// END: Load default motions to set proper order and make sure none are missing
+
                 if (typeof(model3Obj['FileReferences']['Motions']) !== "undefined") {
                     for (let group in model3Obj['FileReferences']['Motions']) {
                         model3Obj['FileReferences']['Motions'][group].forEach((element) => {
@@ -55,11 +88,8 @@ class L2D {
                             if (!motionNames.includes(name+'_'+motionName)){
                                 loader.add(name+'_'+motionName, modelDir+element['File'], { xhrType: PIXI.loaders.Resource.XHR_RESPONSE_TYPE.JSON });
                                 motionNames.push(name+'_'+motionName);
-                            } else {
-                                var n = name+'_'+motionName+String(Date.now());
-                                loader.add(n, modelDir+element['File'], { xhrType: PIXI.loaders.Resource.XHR_RESPONSE_TYPE.JSON });
-                                motionNames.push(name+'_'+motionName);
                             }
+							// removed else statement since it wasn't adding anything of value
                         });
                     }
                 }
@@ -90,7 +120,17 @@ class L2D {
                         let n = element.split(name+'_').pop();
                         motions.set(n, LIVE2DCUBISMFRAMEWORK.Animation.fromMotion3Json(r[element].data));
                     });
-
+		
+					// START: Remove motions that failed to load
+					if (typeof(motions) !== "undefined") {
+						motions.forEach((element, key) => {
+							if (element === null) {
+								motions.delete(key);
+							}
+						});
+					}
+					// END: Remove motions that failed to load
+					
                     let model = null;
                     let coreModel = Live2DCubismCore.Model.fromMoc(moc);
                     if (coreModel == null) {
